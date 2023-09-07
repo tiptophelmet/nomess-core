b5297339e84fd0c3eb2a904973c356b77b98fe76
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-07 16:45:17 +0200

    📦 NEW: Migrate pubsub from nomess-template

---
+++ pubsub/init.go
@@ -0,0 +1,35 @@
+package pubsub
+
+import (
+	"sync"
+
+	"github.com/tiptophelmet/nomess-core/logger"
+	"github.com/tiptophelmet/nomess-core/pubsub/broker"
+)
+
+type pubSubClient struct {
+	broker broker.PubSubBroker
+	mu     sync.Mutex
+}
+
+var client *pubSubClient
+
+func Init(driver, url string) {
+	switch driver {
+	case "redis":
+		client = &pubSubClient{broker: broker.InitRedisBroker()}
+	case "nats":
+		client = &pubSubClient{broker: broker.InitNATSBroker()}
+	default:
+		logger.Panic("unsupported pubsub.driver: %v", driver)
+	}
+
+	client.broker.Connect(url)
+}
+
+func Connection() broker.PubSubBroker {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	return client.broker
+}
