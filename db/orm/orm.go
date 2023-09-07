148572eb3544c44f4c7994bec58303f430a2201c
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-07 16:44:54 +0200

    📦 NEW: Migrate db orm from nomess-template

---
+++ db/orm/orm.go
@@ -0,0 +1,34 @@
+package orm
+
+import (
+	"github.com/tiptophelmet/nomess-core/db/orm/sql"
+	"github.com/tiptophelmet/nomess-core/logger"
+	"gorm.io/driver/clickhouse"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+func Init(driver, dsn string) {
+	var dialector gorm.Dialector
+
+	switch driver {
+	case "mysql":
+	case "tidb":
+		dialector = mysql.Open(dsn)
+	case "postgres":
+		dialector = postgres.Open(dsn)
+	case "sqlite":
+		dialector = sqlite.Open(dsn)
+	case "sqlserver":
+		dialector = sqlserver.Open(dsn)
+	case "clickhouse":
+		dialector = clickhouse.Open(dsn)
+	default:
+		logger.Panic("unsupported db.orm.driver: %v", driver)
+	}
+
+	sql.InitGormConnection(dialector, &gorm.Config{TranslateError: true})
+}
