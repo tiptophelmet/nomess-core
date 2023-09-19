58a5f7ba268411e165ea7afd3c31e888ef0207d4
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-19 21:04:25 +0200

    📦 NEW: Add global middleware register

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,16 +6,23 @@ import (
 
 type MiddlewareFunc func(http.ResponseWriter, *http.Request) (http.ResponseWriter, *http.Request)
 
+var defaultMwList []MiddlewareFunc
+
 var mw = make(map[string][]MiddlewareFunc)
 
 func Register(pattern string, mwFuncList []MiddlewareFunc) {
 	mw[pattern] = mwFuncList
 }
 
+func Default(mwFuncList []MiddlewareFunc) {
+	defaultMwList = mwFuncList
+}
+
 func WithMiddleware(w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request) {
-	mwList, found := mw[r.URL.Path]
-	if !found {
-		return w, r
+	mwList := defaultMwList
+
+	if mwPathList, found := mw[r.URL.Path]; found {
+		mwList = append(mwList, mwPathList...)
 	}
 
 	for _, mw := range mwList {
